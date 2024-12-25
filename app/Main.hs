7fbea886594ba1b09215bc7cbbe8acc73a0aaf74
Simon Johansson <simon@findsourcing.com>
2024-12-25 23:23:04 +0100

    Add syntaxsugar for lambdas

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
 
 import Control.Exception (SomeException, try)
@@ -21,38 +22,62 @@ f (Branch (Branch a b) c) Leaf = a
 f (Branch (Branch a b) c) (Stem x) = f b x
 f (Branch (Branch a b) c) (Branch x y) = f (f c x) y
 
--- newtype Variables = Variables [(String, Tree)]
-
 getValue :: [(String, Tree)] -> String -> Tree
-getValue vars name = fromMaybe (error "No variable with that name") $ lookup name vars
+getValue vars name = fromMaybe (error $ "No variable named " ++ name) $ lookup name vars
 
-parseExpression :: [(String, Tree)] -> [Literal] -> Tree
-parseExpression vars s = let (_, tree) = helper Nothing s in tree
- where
-  maybeF :: Maybe Tree -> Tree -> Tree
-  maybeF Nothing x = x
-  maybeF (Just x) y = f x y
+evalExpression :: [(String, Tree)] -> Syntax -> Tree
+evalExpression vars (Var name) = getValue vars name
+evalExpression vars T = Leaf
+evalExpression vars (Application a b) = f (evalExpression vars a) (evalExpression vars b)
+evalExpression _ other = error $ "Unexpected syntax: " ++ show other
 
-  helper :: Maybe Tree -> [Literal] -> ([Literal], Tree)
-  helper (Just tree) [] = ([], tree)
-  helper Nothing [] = error "Unexpected end of input"
-  helper (Just tree) (EndParen : xs) = (xs, tree)
-  helper Nothing (EndParen : xs) = error "Unexpected closing parenthesis"
-  helper tree (T : xs) = let newTree = maybeF tree Leaf in helper (Just newTree) xs
-  helper tree (StartParen : xs) = let (remaingString, inreTree) = helper Nothing xs in helper (Just (maybeF tree inreTree)) remaingString
-  helper tree (Var v : xs) = let newTree = maybeF tree (getValue vars v) in helper (Just newTree) xs
+evalStatement :: [(String, Tree)] -> Syntax -> (String, Tree)
+evalStatement vars (Assignment name [] expr) = (name, evalExpression vars expr)
 
-parseStatement :: [(String, Tree)] -> [Literal] -> (String, Tree)
-parseStatement vars (Var name : Equals : expr) = (name, parseExpression vars expr)
+evalStatements :: [(String, Tree)] -> [Syntax] -> [(String, Tree)]
+evalStatements = foldl (\acc line -> evalStatement acc line : acc)
 
-parseStatements :: [(String, Tree)] -> [[Literal]] -> [(String, Tree)]
-parseStatements = foldl (\acc line -> parseStatement acc line : acc)
-
-isStatement :: [Literal] -> Bool
-isStatement (Var _ : Equals : _) = True
+isStatement :: Syntax -> Bool
+isStatement (Assignment _ _ _) = True
 isStatement _ = False
 
-data Literal = Var String | T | Equals | StartParen | EndParen
+isUsed :: String -> Syntax -> Bool
+isUsed name (Lambda n s) = name /= n && isUsed name s
+isUsed name (Application a b) = isUsed name a || isUsed name b
+isUsed name (Var n) = name == n
+isUsed name T = False
+
+-- K = t t
+_K = Application T T
+
+-- I = t (t (t t)) t
+_I = Application (Application T (Application T _K)) T
+
+-- S = t (t (t t t)) t
+_S = Application (Application T (Application T (Application _K T))) T
+
+-- B = S (K S) K
+_B = Application (Application _S (Application _K _S)) _K
+
+-- C = S (B B S) (K K)
+_C = Application (Application _S (Application (Application _B _B) _S)) (Application _K _K)
+
+replaceLambdas :: Syntax -> Syntax
+replaceLambdas (Assignment name (var : vars) tree) = replaceLambdas $ Assignment name vars (Lambda var tree)
+replaceLambdas (Assignment name [] tree) = Assignment name [] $ replaceLambdas tree
+replaceLambdas (Application a b) = Application (replaceLambdas a) (replaceLambdas b)
+replaceLambdas (Lambda name tree) | not (isUsed name tree) = Application _K (replaceLambdas tree)
+replaceLambdas (Lambda name T) = T
+replaceLambdas (Lambda name (Var _)) = _I
+replaceLambdas (Lambda name (Application f (Var n))) | name == n && not (isUsed name f) = f
+replaceLambdas (Lambda name (Application f g))
+  | isUsed name f && isUsed name g = Application (Application _S (replaceLambdas (Lambda name f))) (replaceLambdas (Lambda name g))
+  | isUsed name f = Application (Application _C (replaceLambdas (Lambda name f))) g
+  | isUsed name g = Application (Application _B f) (replaceLambdas (Lambda name g))
+replaceLambdas (Lambda name (Lambda other e)) = replaceLambdas (Lambda name (replaceLambdas (Lambda other e)))
+replaceLambdas tree = tree
+
+data Literal = Name String | Equals | StartParen | EndParen | Backslash
   deriving (Show, Eq)
 
 parseLiterals :: String -> [Literal]
@@ -60,14 +85,56 @@ parseLiterals ('#' : xs) = []
 parseLiterals ('(' : xs) = StartParen : parseLiterals xs
 parseLiterals (')' : xs) = EndParen : parseLiterals xs
 parseLiterals ('=' : xs) = Equals : parseLiterals xs
+parseLiterals ('\\' : xs) = Backslash : parseLiterals xs
 parseLiterals (' ' : xs) = parseLiterals xs
 parseLiterals [] = []
 parseLiterals s =
-  let
-    (name, rest) = span (\c -> c /= ' ' && c /= ')') s
-    val = if name == "t" then T else Var name
-   in
-    val : parseLiterals rest
+  let (name, rest) = span (\c -> c /= ' ' && c /= ')') s
+   in (Name name) : parseLiterals rest
+
+data Syntax
+  = Assignment String [String] Syntax
+  | Lambda String Syntax
+  | Application Syntax Syntax
+  | Var String
+  | T
+  deriving (Show)
+
+maybeF :: Maybe Syntax -> Syntax -> Syntax
+maybeF Nothing s = s
+maybeF (Just a) b = Application a b
+
+parseHelper :: Maybe Syntax -> [Literal] -> (Syntax, [Literal])
+parseHelper _ (Equals : _) = error "Unexpected equals sign"
+parseHelper Nothing [] = error "Unexpected end of input"
+parseHelper Nothing (EndParen : _) = error "Unexpected closing parenthesis"
+parseHelper (Just syntax) [] = (syntax, [])
+parseHelper (Just syntax) (EndParen : rest) = (syntax, rest)
+parseHelper ms (Name "t" : rest) = parseHelper (Just $ maybeF ms T) rest
+parseHelper ms (Name name : rest) = parseHelper (Just $ maybeF ms $ Var name) rest
+parseHelper ms (Backslash : Name name : rest)
+  | name == "t" = error "Cannot use reserved name t in lambda"
+  | otherwise =
+      let (innerSyntax, remaining) = parseHelper Nothing rest
+       in parseHelper (Just $ Lambda name innerSyntax) remaining
+parseHelper ms (StartParen : rest) =
+  let (innerSyntax, remaining) = parseHelper Nothing rest
+   in parseHelper (Just $ maybeF ms innerSyntax) remaining
+parseHelper ms lits = error $ show lits
+
+parse :: [Literal] -> Syntax
+parse (Name name : lits)
+  | Equals `elem` lits =
+      let
+        (vars, remaining) = span (/= Equals) lits
+        names = reverse $ map (\(Name n) -> n) vars
+       in
+        case parseHelper Nothing (drop 1 remaining) of
+          (syntax, []) -> replaceLambdas $ Assignment name names syntax
+          (_, lits) -> error $ "Unexpected closing parenthesis before: " ++ show lits
+parse lits = case parseHelper Nothing lits of
+  (syntax, []) -> replaceLambdas syntax
+  (_, lits) -> error $ "Unexpected closing parenthesis before: " ++ show lits
 
 calculateSize :: Tree -> Integer
 calculateSize Leaf = 0
@@ -79,11 +146,11 @@ main = do
   let filename = "example.txt"
   content <- readFile filename
 
-  let allLines = filter (/= []) $ map parseLiterals $ lines content
+  let allLines = map parse $ filter (/= []) $ map parseLiterals $ lines content
   let (statements, expressions) = partition isStatement allLines
 
-  let vars = parseStatements [] statements
-  let results = map (parseExpression vars) expressions
+  let vars = evalStatements [] statements
+  let results = map (evalExpression vars) expressions
   mapM_ print results
 
 trim :: String -> String
@@ -107,12 +174,14 @@ repl = runInputT defaultSettings (loop [])
         outputStrLn ":b - list variables"
         outputStrLn ":l <filename> - load file"
         outputStrLn ":s <expression> - size "
+        outputStrLn ":syn <syntax> - show syntax tree"
+        outputStrLn ":lit <literals> - show literals"
         loop vars
       Just (':' : 'b' : rest) -> do
         let rows :: [String] = map (\(name, tree) -> name ++ " = " ++ show tree) vars
         mapM_ outputStrLn rows
         loop vars
-      Just (':' : 'l' : rest) -> do
+      Just (':' : 'l' : ' ' : rest) -> do
         let filename = trim rest
         result <- liftIO $ try $ readFile filename
         case result of
@@ -120,27 +189,37 @@ repl = runInputT defaultSettings (loop [])
             outputStrLn $ "Error reading file: " ++ show e
             loop vars
           Right content -> do
-            let allLines = filter (/= []) $ map parseLiterals $ lines content
+            let allLines = map parse $ filter (/= []) $ map parseLiterals $ lines content
             let statements = filter isStatement allLines
-            let newVars = parseStatements vars statements
+            let newVars = evalStatements vars statements
             loop newVars
       Just (':' : 's' : rest) -> do
         let literals = parseLiterals rest
         let tree = parseExpression vars literals
         outputStrLn $ "Size: " ++ show (calculateSize tree)
+      Just (':' : 's' : 'y' : 'n' : rest) -> do
+        let literals = parseLiterals rest
+        let syntax = parse literals
+        outputStrLn $ show syntax
+        loop vars
+      Just (':' : 'l' : 'i' : 't' : rest) -> do
+        outputStrLn . show $ parseLiterals rest
         loop vars
       Just input -> do
         let literals = parseLiterals input
-        case literals of
-          [] -> loop vars
-          _ | isStatement literals -> do
-            let (name, tree) = parseStatement vars literals
-            loop ((name, tree) : vars)
-          _ -> do
-            let tree = parseExpression vars literals
-            result <- liftIO $ try $ print tree
-            case result of
-              Left (e :: SomeException) -> do
-                outputStrLn $ "Error evaluating: " ++ show e
-                loop vars
-              Right _ -> loop vars
+        if null literals
+          then loop vars
+          else
+            let syntax = parse literals
+             in if isStatement syntax
+                  then do
+                    let (name, tree) = evalStatement vars syntax
+                    loop ((name, tree) : vars)
+                  else do
+                    let tree = evalExpression vars syntax
+                    result <- liftIO $ try $ print tree
+                    case result of
+                      Left (e :: SomeException) -> do
+                        outputStrLn $ "Error evaluating: " ++ show e
+                        loop vars
+                      Right _ -> loop vars
